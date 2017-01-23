0f7360d7685663f42b7e55c94d342377ce64d9b6
hamub <hbutau35@gmail.com>
2017-01-23 16:45:06 +0200

    Added contributor guidlines

--- talks/models.py
+++ talks/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from markitup.fields import MarkupField
+from django.core.urlresolvers import reverse
 
 class Talk_Type(models.Model):
 
@@ -19,6 +20,12 @@ class Proposal(models.Model):
     title = models.CharField(max_length=1024)
     abstract = MarkupField(help_text="Describe what your talk is about")
     talk_type = models.ForeignKey(Talk_Type)
+    talk_id = models.AutoField(primary_key=True)
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse("home")
+
+
--- talks/views.py
+++ talks/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
-from django.views.generic import (CreateView, TemplateView)
+from django.views.generic import (
+    CreateView,
+    TemplateView,
+    DetailView,
+)
 from talks.models import (Talk_Type, Proposal)
 from talks.forms import ProposalForm
 from .mixins import LoginRequiredMixin
@@ -16,5 +20,17 @@ class CreateTalk(LoginRequiredMixin, CreateView):
 
     def get_contex_data(self, **kwargs):
         context = super(CreateTalk, self).get_context_data(**kwargs)
+        return context
+
+class TalkView(DetailView):
+    model = Proposal
+    template_name = "talks/talk.html"
+
+    def get_object(self,*args, **kwargs):
+        object = super(TalkView, self).get_object(*args, **kwargs)
+        return object
+
+    def get_contex_data(self, **kwargs):
+        context = super(TalkView, self).get_contex_data(**kwargs)
         return context
 
